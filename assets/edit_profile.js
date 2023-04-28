923f2f33d960179cabadb58f3f6ec65b2d3d5d65
Praveenkumar Sekar Nagi <prsekar@fssa.freshworks.com>
2023-04-28 17:06:54 +0530

    feat!: installed eslint

--- assets/edit_profile.js
+++ assets/edit_profile.js
@@ -1,56 +1,54 @@
-function update(e){
-    e.preventDefault();
+function update(e) {
+  e.preventDefault();
 
-    let uniqueID = JSON.parse(localStorage.getItem("uniqueID"));
-    let user_cred = JSON.parse(localStorage.getItem("user_cred"));
+  const uniqueID = JSON.parse(localStorage.getItem("uniqueID"));
+  const user_cred = JSON.parse(localStorage.getItem("user_cred"));
 
-function profile_data(a){
-    return a.email == uniqueID;
+  function profile_data(a) {
+    return a.email === uniqueID;
+  }
+  const person_data = user_cred.find(profile_data);
+
+  const first_name = document.getElementById("first_name").value;
+  const last_name = document.getElementById("last_name").value;
+  const phone = document.getElementById("phone").value;
+  const email = document.getElementById("email").value;
+  const address = document.getElementById("address").value;
+
+  (person_data.first_name = first_name),
+    (person_data.last_name = last_name),
+    (person_data.address = address);
+  person_data.phone = phone;
+  person_data.email = email;
+
+  localStorage.setItem("user_cred", JSON.stringify(user_cred));
+  window.location.href = "./profile.html";
 }
-    person_data = user_cred.find(profile_data)
 
-    let first_name = document.getElementById("first_name").value,
-        last_name = document.getElementById("last_name").value,
-        phone = document.getElementById("phone").value,
-        email = document.getElementById("email").value,
-        address = document.getElementById("address").value;
+function del(e) {
+  e.preventDefault();
 
-        person_data["first_name"] = first_name,
-        person_data["last_name"] = last_name,
-        person_data["address"] = address;
+  if (confirm("Are you sure?")) {
+    const uniqueID = JSON.parse(localStorage.getItem("uniqueID"));
+    const user_cred = JSON.parse(localStorage.getItem("user_cred"));
 
-        localStorage.setItem('user_cred',JSON.stringify(user_cred));
-        window.location.href = "./profile.html";
-}
-
-function del (e) {
-
-
-if(confirm("Are you sure?")){
-    let uniqueID = JSON.parse(localStorage.getItem("uniqueID"));
-    let user_cred = JSON.parse(localStorage.getItem("user_cred"));
-
-    function profile_data(a){
-        return a.email == uniqueID;
+    function profile_data(a) {
+      return a.email === uniqueID;
     }
 
-    person_data = user_cred.find(profile_data);
+    const person_data = user_cred.find(profile_data);
 
-const indexOfUser = user_cred.indexOf(person_data);
-user_cred.splice(indexOfUser,1);
+    const indexOfUser = user_cred.indexOf(person_data);
+    user_cred.splice(indexOfUser, 1);
 
-localStorage.removeItem('uniqueID');
+    localStorage.removeItem("uniqueID");
 
-localStorage.setItem('user_cred',JSON.stringify(user_cred));
-window.location.href = "../../index.html";
-
-};
-
+    localStorage.setItem("user_cred", JSON.stringify(user_cred));
+    window.location.href = "../../index.html";
+  }
 }
 
-function logOut (e) {
-
-    localStorage.removeItem('uniqueID');
-    window.location.href = "../Log in and Sign up/Log in.html";
-
+function logOut(e) {
+  localStorage.removeItem("uniqueID");
+  window.location.href = "../Log in and Sign up/Log in.html";
 }
--- assets/header.js
+++ assets/header.js
@@ -1,58 +1,57 @@
-let origin = window.location.origin;
+const main_source = window.location.origin;
 
-console.log(origin);
-
-let before_login = ` 
+const before_login = ` 
 <header>
        <div class="container">
 <img src="../../assets/Images/v1033-a-15-a-removebg-preview - Copy.svg" width="100px" alt="logo">
 <nav>
     <ul>
-        <li><a href="${origin}/index.html">Home</a></li>
-        <li><a href="${origin}/pages/products/product_list.html">Products</a></li>
-        <li><a href="${origin}/pages/Log%20in%20and%20Sign%20up/Log%20in.html">Log In</a></li>
-        <li><a href="${origin}/pages/Log%20in%20and%20Sign%20up/Log%20in.html"><img src="../../assets/Images/icon-cart.svg" alt="cart"></a></li>
+        <li><a href="${main_source}/index.html">Home</a></li>
+        <li><a href="${main_source}/pages/products/product_list.html">Products</a></li>
+        <li><a href="${main_source}/pages/Log%20in%20and%20Sign%20up/Log%20in.html">Log In</a></li>
+        <li><a href="${main_source}/pages/Log%20in%20and%20Sign%20up/Log%20in.html"><img src="../../assets/Images/icon-cart.svg" alt="cart"></a></li>
     </ul>
 </nav>
 </header>`;
 
-let after_login = `
+const after_login = `
 <header>
         <div class="container">
 <img src="../../assets/Images/v1033-a-15-a-removebg-preview - Copy.svg" width="100px" alt="logo">
 <nav>
     <ul>
-        <li><a href="${origin}/index.html">Home</a></li>
-        <li><a href="${origin}/pages/products/product_list.html">Products</a></li>
-        <li><a href="${origin}/pages/Cart/cart.html"><img src="../../assets/Images/icon-cart.svg" alt="cart"></a></li>
-        <li><a href="${origin}/pages/Profile/profile.html"><img src="../../assets/Images/profile.svg" width="26px"
+        <li><a href="${main_source}/index.html">Home</a></li>
+        <li><a href="${main_source}/pages/products/product_list.html">Products</a></li>
+        <li><a href="${main_source}/pages/Cart/cart.html"><img src="../../assets/Images/icon-cart.svg" alt="cart"></a></li>
+        <li><a href="${main_source}/pages/Profile/profile.html"><img src="../../assets/Images/profile.svg" width="26px"
                     alt="profile"></a></li>
     </ul>
 </nav>
-</header>`
+</header>`;
 
-let user_login_id = JSON.parse(localStorage.getItem('uniqueID'));
+const user_login_id = JSON.parse(localStorage.getItem("uniqueID"));
 
 console.log(user_login_id);
 
 if (user_login_id) {
-    document.body.insertAdjacentHTML("afterbegin", after_login);
+  document.body.insertAdjacentHTML("afterbegin", after_login);
 
-    let log_out = document.getElementById("logOut")
+  const log_out = document.getElementById("logOut");
 
-    log_out?.addEventListener("click",() => {
-        if(confirm("Are you sure you want to log out")) {
-        localStorage.removeItem('uniqueID');
-        document.body.innerHTML = before_login;
+  log_out?.addEventListener("click", () => {
+    if (confirm("Are you sure you want to log out")) {
+      localStorage.removeItem("uniqueID");
+      document.body.innerHTML = before_login;
     }
-})
-}
+  });
+} else {
+  document.body.insertAdjacentHTML("afterbegin", before_login);
 
-else {
-    document.body.insertAdjacentHTML("afterbegin", before_login);
+  const log_out = document.getElementById("logOut");
 
-    let log_out = document.getElementById("logOut")
-
-    log_out?.removeEventListener("click",() => document.body.innerHTML = after_login);
-    localStorage.removeItem('uniqueID');
+  log_out?.removeEventListener(
+    "click",
+    () => (document.body.innerHTML = after_login)
+  );
+  localStorage.removeItem("uniqueID");
 }
